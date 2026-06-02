06c1a2207255ffa2c80981353b0e48a23da0404a
DaLama007 <marwan.lamesch@gmail.com>
2026-06-02 23:48:39 +0200

    Define structs as type for the needed things like vectors, rays, points. Fix minor chronology mistakes. Add a rect drawing function and draw an example rect.

--- main.c
+++ main.c
@@ -10,6 +10,37 @@ struct shape{
   int endX;
   int endY;
 };
+
+typedef struct{
+  int x;
+  int y;
+}Point;
+
+typedef struct{
+  int x;
+  int y;
+}Vector;
+
+typedef struct{
+  Point origin;
+  Vector direction;
+}Ray;
+
+void DrawRect(SDL_Renderer * renderer, Point topLeft, int32_t width, int32_t height)
+{
+  int x = topLeft.x;
+  int y = topLeft.y;
+  for(int i = x ; i< x+width; i++){
+    SDL_RenderDrawPoint(renderer, i, y);
+    SDL_RenderDrawPoint(renderer, i , y+height);
+  }
+
+  for(int i = y ; i< y+height; i++){
+    SDL_RenderDrawPoint(renderer, x, i);
+    SDL_RenderDrawPoint(renderer, x+width , i);
+  }
+}
+
 void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius)
 {
 
@@ -36,13 +67,13 @@ int main(int argc, char *argv[])
   }
 
   SDL_Window *window = SDL_CreateWindow(
-      "RayTracing Engine",
-      SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED,
-      640,
-      480,
-      SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
-      );
+    "RayTracing Engine",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    640,
+    480,
+    SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+  );
 
   if (!window) {
     printf("SDL_CreateWindow error: %s\n", SDL_GetError());
@@ -53,10 +84,10 @@ int main(int argc, char *argv[])
   int running = 1;
   SDL_Event e;
   SDL_Renderer *renderer = SDL_CreateRenderer(
-      window,
-      -1,
-      SDL_RENDERER_ACCELERATED
-      );
+    window,
+    -1,
+    SDL_RENDERER_ACCELERATED
+  );
 
 
   int pw, ph;
@@ -64,6 +95,23 @@ int main(int argc, char *argv[])
   int centerY = 300;
   int centerX = 300;
   printf("Pixel size: %dx%d\n", pw, ph);
+
+  // get sdl surface
+  SDL_Surface* pSurface = NULL;
+  pSurface = SDL_GetWindowSurface(window);
+
+  // create color bitmap
+  Uint32 color =SDL_MapRGB(pSurface->format, 255, 255, 255);
+  //create rect at fixed pos 
+  SDL_Rect rect;
+
+  //set pos for different rect cords
+  Point tL = {20,20};
+  int width = 100;
+  int height = 100;
+  DrawRect(renderer, tL, width, height);
+  DrawCircle(renderer,centerX,centerY,100);
+
   while (running) {
     while (SDL_PollEvent(&e)) {
       if (e.type == SDL_QUIT) {
@@ -73,7 +121,6 @@ int main(int argc, char *argv[])
         if(e.key.keysym.sym == SDLK_UP){
           centerY-=20;
         }
-
         else if(e.key.keysym.sym == SDLK_DOWN){
           centerY+=20;
         }
@@ -87,12 +134,15 @@ int main(int argc, char *argv[])
 
     }
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    //SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    DrawCircle(renderer,centerX,centerY,100);
+
+    DrawRect(renderer, tL, width, height);
+    DrawCircle(renderer, centerX, centerY, 50); 
+
     SDL_RenderPresent(renderer);
   }
-  
+
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
