885f88d0b6d15905bc47cff4c3649e93e333cc2d
DaLama007 <marwan.lamesch@gmail.com>
2026-06-08 23:13:32 +0200

    Simplify shape generation and randomize it even for circles

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct{
   char* type;
@@ -43,10 +44,8 @@ void DrawRect(SDL_Renderer * renderer, Point topLeft, int32_t width, int32_t hei
   }
 }
 
-void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius, Ray *rays, int num_rays)
+void DrawSource(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius, Ray *rays, int num_rays)
 {
-
-
   for (int i = -90; i < num_rays; i++) {
     float angle = 2.0 * M_PI * i / num_rays; // radians
     int x = (int)(centreX+ radius * cos(angle));
@@ -61,6 +60,15 @@ void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32
   }
 }
 
+void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius)
+{
+  for (int i = -90; i < 360; i++) {
+    float angle = 2.0 * M_PI * i / 360; // radians
+    int x = (int)(centreX+ radius * cos(angle));
+    int y = (int)(centreY + radius * sin(angle));
+    SDL_RenderDrawPoint(renderer, x, y); // Draw point
+  }
+}
 
 void DrawRays(SDL_Renderer * renderer, Ray *rays, int num_rays, Shape *shapes, int num_shapes, double time, double max_time)
 {
@@ -69,29 +77,38 @@ void DrawRays(SDL_Renderer * renderer, Ray *rays, int num_rays, Shape *shapes, i
     int originX = rays[i].origin.x;
     int originY = rays[i].origin.y;
 
-    
+
     //calculate entering and exiting times between x1 and x2 of shapes
     double minT = time;
     for (int j = 0; j< num_shapes; j++) {
-      double tendX = (shapes[j].endX - originX)/ rays[i].direction.x;
-      double tstartX = (shapes[j].startX - originX)/ rays[i].direction.x;
-      double tminX = fmin(tstartX, tendX);
-      double tmaxX= fmax(tstartX, tendX);
-      double tendY = (shapes[j].endY - originY)/ rays[i].direction.y;
-      double tstartY = (shapes[j].startY - originY)/ rays[i].direction.y;
-      double tmaxY = fmax(tstartY, tendY);
-      double tminY = fmin(tstartY, tendY);
       
-      // calc entry and exit times 
-      double tentry = fmax(tminX, tminY);
-      double texit = fmin(tmaxX, tmaxY);
+      Shape curr = shapes[j];
+      // check for rectangle intersection
+      if(strcmp("rectangle", curr.type)==0){
+        double tendX = (curr.endX - originX)/ rays[i].direction.x;
+        double tstartX = (curr.startX - originX)/ rays[i].direction.x;
+        double tminX = fmin(tstartX, tendX);
+        double tmaxX= fmax(tstartX, tendX);
+        double tendY = (curr.endY - originY)/ rays[i].direction.y;
+        double tstartY = (curr.startY - originY)/ rays[i].direction.y;
+        double tmaxY = fmax(tstartY, tendY);
+        double tminY = fmin(tstartY, tendY);
 
-      // check if entry time is smaller than exit time
-      if(tentry<=texit && tentry>=0){
-        minT= fmin(minT, tentry);
+        // calc entry and exit times 
+        double tentry = fmax(tminX, tminY);
+        double texit = fmin(tmaxX, tmaxY);
+
+        // check if entry time is smaller than exit time
+        if(tentry<=texit && tentry>=0){
+          minT= fmin(minT, tentry);
+        }
+      }
+
+      else{
+        // circle is defined by (x-C_x)^2 + (y-C_y)^2 = r^2
       }
     }
-    
+
     // either draw with the time of collision or with the basic value
     for (int j = 0;j<minT;j++) {
       int x = rays[i].origin.x + j*rays[i].direction.x;
@@ -105,12 +122,47 @@ void DrawRays(SDL_Renderer * renderer, Ray *rays, int num_rays, Shape *shapes, i
     }
   }
 }
+
+void generateShapes(SDL_Renderer * renderer, Shape *shapes, int shapeCount, int panelWidth, int panelHeight){
+  for (int i=0;i<shapeCount;i++) {
+    int width = rand()%300;
+    int height = rand()%300;
+    int startX = rand()%(panelWidth - width);
+    int startY = rand()%(panelHeight - height);
+    int endX = startX+width;
+    int endY = startY+height;
+
+    // select one of the available shapes randomly
+    int det = rand()%3;
+    if(det == 0) shapes[i] = (Shape){"rectangle", startX, startY, endX, endY};
+    else shapes[i] = (Shape){"circle", startX, startY, endX, endY};
+  }
+}
+
+void drawShapes(SDL_Renderer * renderer, Shape *shapes, int shapeCount){
+  for (int i=0;i<shapeCount;i++) {
+    Shape curr = shapes[i];
+    if(strcmp(curr.type, "circle")){
+      int radius = (curr.endX -curr.startX)/2;
+      int centerX = curr.startX+radius;
+      int centerY = curr.startY+radius;
+      DrawCircle(renderer, centerX,centerY,radius);
+    }
+    else{
+      Point start = {curr.startX, curr.startY};
+      int width = curr.endX-curr.startX;
+      int height = curr.endY - curr.startY;
+      DrawRect(renderer, start, width, height);
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   (void)argc;
   (void)argv;
   int num_rays = 360;
-  int num_shapes = 1;
+  int num_shapes = 4;
   double time_max = 300;
   Ray *rays = malloc(num_rays * sizeof(Ray));
   Shape *shapes = malloc(num_shapes * sizeof(Shape));
@@ -157,20 +209,11 @@ int main(int argc, char *argv[])
 
   // create color bitmap
   Uint32 color =SDL_MapRGB(pSurface->format, 255, 255, 255);
+
   //create rect at fixed pos 
   SDL_Rect rect;
 
-
-  // generate pos for different rect cords
-  int width = rand()%300;
-  int height = rand()%300;
-  int startX = rand()%(pw - width);
-  int startY = rand()%(ph - height);
-  int endX = startX+width;
-  int endY = startY+height;
-  shapes[0] = (Shape){"rect", startX, startY, endX, endY};
-  Point tL = {startX,startY};
-
+  generateShapes(renderer, shapes, num_shapes, pw, ph);
 
   while (running) {
     // increase time if max not reached
@@ -202,8 +245,10 @@ int main(int argc, char *argv[])
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    DrawRect(renderer, tL, width, height);
-    DrawCircle(renderer, centerX, centerY, 50, rays, num_rays);
+    // shapes and light source
+    SDL_SetRenderDrawColor(renderer,255,255,255, 255);
+    drawShapes(renderer, shapes, num_shapes);
+    DrawSource(renderer, centerX, centerY, 50, rays, num_rays);
     DrawRays(renderer, rays, num_rays, shapes, num_shapes, time, time_max);
 
     SDL_RenderPresent(renderer);
